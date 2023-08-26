f70a9a489bb4904aafc24dc4904ca666dc13677a
eyduh <29815625@users.noreply.github.com>
2023-08-26 23:22:50 +0200

    builds now, just gotta fix installPhase

--- flake.nix
+++ flake.nix
@@ -1,19 +1,53 @@
 {
   description = "A flake for bmusb by Sesse";
 
-  inputs = {
+  inputs = {       
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";    
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
-    };
-    
-  }
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    }; 
   };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        baseDependencies = with pkgs; [
+          libusb1.dev
+          pkg-config
+        ];
+        bmusb = (with pkgs; stdenv.mkDerivation {
+          pname = "bmusb";
+          version = "0.7.6";
+          src = fetchgit {
+            url = "http://git.sesse.net/bmusb";
+            rev = "6334dc3fcbd67a757f6065a946a2343185c233fc";
+            sha256 = "sha256-bK/VaszIzayjft6wIjuhdw78NsBO+N27hSED/4OJT78=";
+          };
+          buildInputs = baseDependencies;
+          #buildPhase = "make -j $NIX_BUILD_CORES";
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/usr/local/lib/pkgconfig \
+            mkdir -p $out/usr/local/include/bmusb \
+            mkdir -p $out/lib/udev/rules.d
+          '';
+        }
+      );
+      in rec {
+        apps.default = flake-utils.lib.mkApp {
+          drv = packages.default;
+        };
+        packages.default = bmusb;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            baseDependencies
+          ];
+        };
+      }
+    );
 }
